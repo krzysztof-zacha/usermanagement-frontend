42bb6f920e581b9a7460be680dc3c2eb3b6a33ae
Krzysztof Zacha <zacha.krzysztof@gmail.com>
2019-10-23 22:49:38 +0200

    Navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AboutComponent} from './about/about.component';
+import {UserListComponent} from './user/user-list/user-list.component';
+import {UserDetailComponent} from './user/user-detail/user-detail.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MenuComponent} from './components/menu/menu.component';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     UserListComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
