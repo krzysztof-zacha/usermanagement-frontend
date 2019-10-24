3749b80f95dc706a2bf0dbdeb19d6781a8eea982
Krzysztof Zacha <zacha.krzysztof@gmail.com>
2019-10-24 22:08:45 +0200

    Add required validators

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -2,7 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {User} from "../user";
 import {UserService} from "../user.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-detail',
@@ -20,12 +20,12 @@ export class UserDetailComponent {
     dialogRef.disableClose = true;
     this.userId = data.id;
     this.userForm = new FormGroup({
-      username: new FormControl(data.username),
-      name: new FormControl(data.name),
-      surname: new FormControl(data.surname),
-      role: new FormControl(data.role),
-      email: new FormControl(data.email),
-      enabled: new FormControl(data.enabled),
+      username: new FormControl(data.username, [Validators.required]),
+      name: new FormControl(data.name, [Validators.required]),
+      surname: new FormControl(data.surname, [Validators.required]),
+      role: new FormControl(data.role, [Validators.required]),
+      email: new FormControl(data.email, [Validators.required]),
+      enabled: new FormControl(data.enabled, [Validators.required]),
     });
 
   }
@@ -33,8 +33,13 @@ export class UserDetailComponent {
   save() {
     const user = this.userForm.value;
     user.id = this.userId;
-    this.userService.save(user)
-      .subscribe(() => this.dialogRef.close());
+    Object.keys(this.userForm.controls).forEach(key => {
+      this.userForm.get(key).markAsTouched();
+    });
+    if (this.userForm.valid) {
+      this.userService.save(user)
+        .subscribe(() => this.dialogRef.close());
+    }
   }
 
   cancel() {
